d948202fd0149fb43af251947211454dd0ea896d
mikecirone <michael@ballparq.io>
2018-04-04 11:29:12 -0700

    made skeleton from portfolio code; this does not have mongodb or hot-reloading

--- client/app.js
+++ client/app.js
@@ -1,36 +1,27 @@
 var angular = require('angular')
 
-require('./base.scss')
-
 var app = angular.module('app', [
     require('angular-ui-router')
 ])
 
-app.directive('exampleSection', function() {
-    return {
-        restrict: 'E',
-        template: '<p>Lorem ipsum dolorum.</p>'
-    }
-})
-
 app.config(
     function($urlRouterProvider, $stateProvider, $locationProvider){
-        $stateProvider
-            .state('exampleSection', {
-                url: '/main',
-                views: {
-                    content: {
-                        template: require('./content.html')
-                    }
-                }
-            })
-    $urlRouterProvider.otherwise('/main')
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    })
-})
 
-app.controller("main", function($scope, $state) {
-    $scope.$state = $state
-})
+        $urlRouterProvider.otherwise('/')
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        })
+
+        $stateProvider
+        .state('app', {
+            url: '/',
+            views: {
+                header: {
+                    template: require('./header/header.html')
+                }
+            }
+        })
+
+    }
+)
--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,9 @@ module.exports = {
   entry: [
     './client/app.js'
   ],
-  devServer: {
-    contentBase: './client/public',
-    historyApiFallback: true
-  },
   output: {
-    path: path.resolve(__dirname, "client/public"),
-    publicPath: "/assets/",
-    filename: "bundle.js"
+    path: __dirname,
+    filename: './client/public/bundle.js'
   },
   plugins: [
     new webpack.ProvidePlugin({
@@ -24,21 +19,7 @@ module.exports = {
     {
         test: /\.html$/,
         loader: 'raw-loader'
-    },
-    {
-        test: /\.scss$/,
-        use: [{
-            loader: "style-loader"
-        }, {
-            loader: "css-loader"
-        }, {
-            loader: "sass-loader",
-            options: {
-                includePaths: [path.resolve(__dirname, './client')]
-            }
-        }]
-    }
-    ],
+    }],
     loaders: [
       {
         loader: 'babel-loader',
