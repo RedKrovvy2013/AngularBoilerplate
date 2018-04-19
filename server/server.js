81654e1764646027d00db09c7430eba03e165ad7
mikecirone <michael@ballparq.io>
2018-04-18 17:00:39 -0700

    minor, tweak

--- server/server.js
+++ server/server.js
@@ -5,5 +5,9 @@ var app = express()
 
 app.use(express.static(path.join(__dirname, '../client/public')))
 
+app.get('*', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../client/public/index.html'))
+})
+
 const port = process.argv[2] ? process.argv[2] : 8080
 const server = app.listen(port, function() {})
